dc17f07805ba9c9cf180093f153c516dc0738099
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-01-30 23:46:16 +0545

    Setup datautility package

---
+++ datautility/__init__.py
@@ -0,0 +1,20 @@
+from os import environ
+from logging import INFO, DEBUG, ERROR, WARNING, CRITICAL
+
+
+name = 'datautility'
+version = '1.0.0-alpha.1'
+
+log_level_map = {
+    'info': INFO,
+    'debug': DEBUG,
+    'error': ERROR,
+    'warning': WARNING,
+    'critical': CRITICAL
+}
+config = {
+    'logging': {
+        'level': log_level_map.get(environ.get('LOG_LEVEL')) or DEBUG,
+        'format': '%(asctime)s - [ %(levelname)s ] %(name)s - %(message)s'
+    }
+}
